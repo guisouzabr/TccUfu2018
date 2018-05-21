b77aa0830dd7d2701be703fd42ec55bee3f04488
Gustavo de Souza Silva <xgustavossilva@gmail.com>
2018-05-21 17:46:06 -0300

    [Add] crossplataforminput

---
+++ Assets/_Project/Scripts/InGameScripts/Map1 and Map2 Scripts/btnStartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnStartGame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void iniciarJogo()
+    {
+        SceneManager.LoadScene("mapbin1");
+    }
+
+    public void iniciarJogo2()
+    {
+        SceneManager.LoadScene("map2");
+    }
+}
