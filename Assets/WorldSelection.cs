cb35c730caaf998e8fca358792abc516c2e4737a
Gustavo Silva <xgustavossilva@gmail.com>
2018-06-20 21:28:40 -0300

    [Add] World Selection script and some test sprites

---
+++ Assets/WorldSelection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class WorldSelection : MonoBehaviour {
+
+	public Button[] worlds;
+
+	int actualWorld;
+
+	private void Awake() {
+		if(PlayerPrefs.HasKey("actualWorld")){
+			actualWorld = PlayerPrefs.GetInt("actualWorld");
+		}else{
+			PlayerPrefs.SetInt("actualWorld",0);
+		}
+		PlayerPrefs.SetInt("actualWorld",1);
+		actualWorld = PlayerPrefs.GetInt("actualWorld");
+	}
+
+	// Use this for initialization
+	void Start () {
+		if(actualWorld != 0){
+			for(int i=1;i<actualWorld+1;i++){
+				worlds[i].enabled = true;
+				worlds[i].gameObject.GetComponent<Image>().color = Color.green;
+			}
+		}
+	}
+
+
+	public void LoadWorld(string worldName){
+		SceneManager.LoadScene(worldName);
+	}
+}
--- Assets/_Project/Scripts/MainMenu/MainMenuButtons.cs
+++ Assets/_Project/Scripts/MainMenu/MainMenuButtons.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void LoadAdditive(string sceneName){
+		SceneManager.LoadScene(sceneName,LoadSceneMode.Additive);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
 	}
 }
