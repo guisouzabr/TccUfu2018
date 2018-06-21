ecdc21458cbb14a351f1886842134af2de1574ad
Gustavo Silva <xgustavossilva@gmail.com>
2018-06-20 21:33:51 -0300

    [Add] simple world selection system

--- Assets/WorldSelection.cs
+++ Assets/WorldSelection.cs
@@ -5,22 +5,20 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class WorldSelection : MonoBehaviour {
 
-	public Button[] worlds;
+	public Button[] worlds; //All the world buttons
 
-	int actualWorld;
+	public int actualWorld; //Public only for test, u cant change in the final version
 
 	private void Awake() {
+		//Test the actualWorld key and get that.
 		if(PlayerPrefs.HasKey("actualWorld")){
 			actualWorld = PlayerPrefs.GetInt("actualWorld");
 		}else{
 			PlayerPrefs.SetInt("actualWorld",0);
 		}
-		PlayerPrefs.SetInt("actualWorld",1);
-		actualWorld = PlayerPrefs.GetInt("actualWorld");
 	}
-
-	// Use this for initialization
 	void Start () {
+		//Enable the world that the player can go
 		if(actualWorld != 0){
 			for(int i=1;i<actualWorld+1;i++){
 				worlds[i].enabled = true;
@@ -29,7 +27,10 @@ public class WorldSelection : MonoBehaviour {
 		}
 	}
 
-
+	/// <summary>
+	/// Load a world
+	/// </summary>
+	/// <param name="worldName">World to load</param>
 	public void LoadWorld(string worldName){
 		SceneManager.LoadScene(worldName);
 	}
